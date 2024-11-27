82e381d11f6d5e81f43b05e6be7ac663c91b52e4
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2024-11-27 19:23:54 +0100

    Create firebase.ts

---
+++ src/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDWvMx1VHkKhBZGV7mMyr29GvIer0f5UV8",
+  authDomain: "talking-34f62.firebaseapp.com",
+  databaseURL: "https://talking-34f62-default-rtdb.firebaseio.com",
+  projectId: "talking-34f62",
+  storageBucket: "talking-34f62.appspot.com",
+  messagingSenderId: "24426648864",
+  appId: "1:24426648864:web:9b85eb96c9e788e0d87345",
+  measurementId: "G-64N0P575VV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
