f8a3c900d4879c87637e1e4c1375fff3960d6d7e
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2024-11-30 08:05:33 +0100

    Create layout.tsx

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,17 @@
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <div className={styles.container}>
+    <div className={styles.left}>
+      <header className={styles.logo}>
+        <BriefcaseBusiness height={32} width={32} /> <h1>Jobpilot</h1>
+      </header>
+      <main className={styles.main}>{children}</main>
+        </div>
+        <div className={styles.background}>
+          <Image src={'/logoBackground.png'} alt='Background' fill priority/>
+        </div>
+    </div>;
+}
