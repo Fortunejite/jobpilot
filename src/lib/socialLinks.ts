46595628d6c344d399dbdf34b10730facb38e805
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2024-12-01 05:37:42 +0000

    added data library

---
+++ src/lib/socialLinks.ts
@@ -0,0 +1,32 @@
+import {
+    Facebook,
+    Instagram,
+    Linkedin,
+    Twitter,
+    Youtube,
+  } from 'lucide-react';
+
+const links = [
+    {
+      name: 'Facebook',
+      icon: <Facebook />,
+    },
+    {
+      name: 'LinkedIn',
+      icon: <Linkedin />,
+    },
+    {
+      name: 'Twitter',
+      icon: <Twitter />,
+    },
+    {
+      name: 'Instagram',
+      icon: <Instagram />,
+    },
+    {
+      name: 'Youtube',
+      icon: <Youtube />,
+    },
+  ];
+
+  export default links
