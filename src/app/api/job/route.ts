0b6ebe7708e537b730fa94d213bb3ff27c1ab182
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2024-12-02 18:02:08 +0100

    Update route.ts

--- src/app/api/job/route.ts
+++ src/app/api/job/route.ts
@@ -1,10 +1,43 @@
 import { auth } from '@/auth';
+import dbConnect from '@/lib/mongodb';
 import Job from '@/models/job';
 import { NextResponse } from 'next/server';
 import { ZodError } from 'zod';
 
+export async function GET(request: NextRequest) {
+  try {
+    await dbConnect();
+
+    const page = Number(request.nextUrl.searchParams.get('page')) || 1;
+    const limit = Number(request.nextUrl.searchParams.get('limit')) || 10;
+    const companyId = request.nextUrl.searchParams.get('companyId') || null
+    const skip = (page - 1) * limit;
+
+    if (companyId) {
+      const job = await Job.findOne({companyId})
+      .populate('companyId');
+
+      if (!job) return NextResponse.json({ data: null }, { status: 404 });
+
+    return NextResponse.json({ data: jobs }, { status: 200 });
+    }
+    const jobs = await Job.find()
+      .populate('companyId')
+      .skip(skip)
+      .limit(limit);
+
+    if (!jobs) return NextResponse.json({ data: null }, { status: 404 });
+
+    return NextResponse.json({ data: jobs }, { status: 200 });
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({ data: null }, { status: 500 });
+  }
+}
+
 export async function POST(request: Request) {
   try {
+    await dbConnect();
     const data = await request.json();
     const session = await auth();
     const user = session?.user;
